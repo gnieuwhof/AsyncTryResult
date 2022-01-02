ea9cabfe26b2e52b1b0025c13001d4320dcee774
Guido Nieuwhof <guido.nieuwhof@gmail.com>
2022-01-02 01:20:12 +0100

    Added TryGetResult class.

--- AsyncTryResult/Program.cs
+++ AsyncTryResult/Program.cs
@@ -1,15 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AsyncTryResult
+﻿namespace AsyncTryResult
 {
+    using System;
+    using System.Threading.Tasks;
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Main().GetAwaiter().GetResult();
+
+            Console.ReadKey();
+        }
+
+        private static async Task Main()
+        {
+            TryGetResult<string> result = await TryGetString();
+
+            if (result.Success)
+            {
+                Console.WriteLine(result.Result);
+            }
+            else
+            {
+                Console.WriteLine("TryGetString failed!");
+            }
+        }
+
+        private static async Task<TryGetResult<string>> TryGetString()
+        {
+            // Simulate slow operation...
+            await Task.Delay(100);
+
+            var random = new Random(Guid.NewGuid().GetHashCode());
+
+            TryGetResult<string> tryGetResult;
+
+            // We only succeed half of the time.
+            if (random.Next(2) == 1)
+            {
+                tryGetResult = TryGetResult<string>.Succeeded("Hello, World!");
+
+                return tryGetResult;
+            }
+
+            tryGetResult = TryGetResult<string>.Failed();
+
+            return tryGetResult;
         }
     }
 }
---
+++ AsyncTryResult/TryGetResult.cs
@@ -0,0 +1,33 @@
+﻿namespace AsyncTryResult
+{
+    public class TryGetResult<T>
+    {
+        public bool Success { get; }
+
+        public T Result { get; set; }
+
+
+        private TryGetResult(bool success)
+        {
+            this.Success = success;
+        }
+
+
+        public static TryGetResult<T> Succeeded(T result)
+        {
+            var tryGetResult = new TryGetResult<T>(true)
+            {
+                Result = result
+            };
+
+            return tryGetResult;
+        }
+
+        public static TryGetResult<T> Failed()
+        {
+            var tryGetResult = new TryGetResult<T>(false);
+
+            return tryGetResult;
+        }
+    }
+}
