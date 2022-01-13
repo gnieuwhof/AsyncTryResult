f3218fbd4760f5b2d3e0d236ca577d5bebed1ee9
Guido Nieuwhof <guido.nieuwhof@gmail.com>
2022-01-13 23:38:52 +0100

    Added Error object.

--- AsyncTryResult/Program.cs
+++ AsyncTryResult/Program.cs
@@ -12,9 +12,45 @@
             Console.ReadKey();
         }
 
+        //private static async Task Main()
+        //{
+        //    TryGetResult<string> result = await TryGetString1();
+
+        //    if (result.Success)
+        //    {
+        //        Console.WriteLine(result.Result);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("TryGetString failed!");
+        //    }
+        //}
+
+        //private static async Task<TryGetResult<string>> TryGetString1()
+        //{
+        //    // Simulate slow operation...
+        //    await Task.Delay(100);
+
+        //    var random = new Random(Guid.NewGuid().GetHashCode());
+
+        //    TryGetResult<string> tryGetResult;
+
+        //    // We only succeed half of the time.
+        //    if (random.Next(2) == 1)
+        //    {
+        //        tryGetResult = TryGetResult<string>.Succeeded("Hello, World!");
+
+        //        return tryGetResult;
+        //    }
+
+        //    tryGetResult = TryGetResult<string>.Failed();
+
+        //    return tryGetResult;
+        //}
+
         private static async Task Main()
         {
-            TryGetResult<string> result = await TryGetString();
+            TryGetResult<string, string> result = await TryGetString2();
 
             if (result.Success)
             {
@@ -22,28 +58,30 @@
             }
             else
             {
-                Console.WriteLine("TryGetString failed!");
+                Console.WriteLine(result.Error);
             }
         }
 
-        private static async Task<TryGetResult<string>> TryGetString()
+        private static async Task<TryGetResult<string, string>> TryGetString2()
         {
             // Simulate slow operation...
             await Task.Delay(100);
 
             var random = new Random(Guid.NewGuid().GetHashCode());
 
-            TryGetResult<string> tryGetResult;
+            TryGetResult<string, string> tryGetResult;
 
             // We only succeed half of the time.
             if (random.Next(2) == 1)
             {
-                tryGetResult = TryGetResult<string>.Succeeded("Hello, World!");
+                tryGetResult = TryGetResult<string, string>
+                    .Succeeded("Hello, World!");
 
                 return tryGetResult;
             }
 
-            tryGetResult = TryGetResult<string>.Failed();
+            tryGetResult = TryGetResult<string, string>
+                .Failed("TryGetString failed!");
 
             return tryGetResult;
         }
--- AsyncTryResult/TryGetResult.cs
+++ AsyncTryResult/TryGetResult.cs
@@ -33,4 +33,45 @@
             return tryGetResult;
         }
     }
+
+
+    public sealed class TryGetResult<TSuccess, TFailed>
+    {
+        private TryGetResult()
+        {
+            this.Success = false;
+        }
+
+        private TryGetResult(TSuccess result)
+        {
+            this.Success = true;
+            this.Result = result;
+        }
+
+
+        public bool Success { get; }
+
+        public TSuccess Result { get; }
+         
+        public TFailed Error { get; private set; }
+
+
+        public static TryGetResult<TSuccess, TFailed>
+            Succeeded(TSuccess result)
+        {
+            var tryGetResult = new TryGetResult<TSuccess, TFailed>(result);
+
+            return tryGetResult;
+        }
+
+        public static TryGetResult<TSuccess, TFailed> Failed(TFailed error)
+        {
+            var tryGetResult = new TryGetResult<TSuccess, TFailed>()
+            {
+                Error = error
+            };
+
+            return tryGetResult;
+        }
+    }
 }
